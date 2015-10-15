723a1c5e21e5e8df770bed9e073f416297f463b5
sara5 <mmsara92@gmail.com>
2015-10-15 00:23:03 -0700

    lista_recetas

--- topchef/recetas/views.py
+++ topchef/recetas/views.py
@@ -5,4 +5,4 @@ from recetas.models import Receta
 
 def indice(request):
    lista_recetas = Receta.objects.all()
-   return render(request, 'index.html', {'lista_recetas': lista_recetas })
+   return render(request, 'recetas/index.html', {'lista_recetas': lista_recetas })
